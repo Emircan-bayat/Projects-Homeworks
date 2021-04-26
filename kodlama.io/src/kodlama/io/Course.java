042ac4945da179cf62b1ae88b5eee860fab01393
Emircan Bayat <82971408+Emircan-bayat@users.noreply.github.com>
2021-04-26 04:37:07 +0300

    Add files via upload

---
+++ kodlama.io/src/kodlama/io/Course.java
@@ -0,0 +1,17 @@
+package kodlama.io;
+
+public class Course {
+
+	public Course(String courseName, String courseInstructor, double coursePrice) {
+
+		this.courseName = courseName;
+		this.courseInstructor = courseInstructor;
+		this.coursePrice = coursePrice;
+
+	}
+
+	String courseName;
+	String courseInstructor;
+	double coursePrice;
+
+}
---
+++ kodlama.io/src/kodlama/io/CourseManager.java
@@ -0,0 +1,14 @@
+package kodlama.io;
+
+public class CourseManager {
+
+	public void addToMyCourses (Course course) {
+		System.out.println("Kurslarýma eklendi:"+course.courseName);
+		
+	}
+	public void removeCourse (Course course) {
+		System.out.println("Kurslarýmdan kaldýrýldý:"+course.courseName);
+	
+	
+}
+}
